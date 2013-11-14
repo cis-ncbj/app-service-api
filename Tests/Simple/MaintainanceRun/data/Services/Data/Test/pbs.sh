b5f2b4a27b3cd0929d52a886d0eb0d53ecb60796
Konrad Klimaszewski <graag@o2.pl>
2013-11-14 10:56:11 +0100

    Tests update.
    - Tests updated to run with SSH scheduler.
    - Tests updated for changes introduced in job state refactor.

--- Tests/Simple/MaintainanceRun/data/Services/Data/Test/pbs.sh
+++ Tests/Simple/MaintainanceRun/data/Services/Data/Test/pbs.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
-#PBS -q test_slc6
-#
+#PBS -q @@{CIS_QUEUE}
+#@@{PBS_OPTS}
 
-echo "Commencing test: CleanRun" | tee -a progress.log
-sleep 1
+echo "Commencing test: @@{TestName}" | tee -a progress.log
+sleep @@{SLEEP}
 echo 10 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 9 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 8 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 7 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 6 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 5 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 4 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 3 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 2 | tee -a progress.log
-sleep 1
+sleep @@{SLEEP}
 echo 1 | tee -a progress.log
 
-python -c "print 1.0 / 20.99"
-/bin/ls
+python -c "print @@{1Float} / @@{2Float}"
+@@{COMMAND}
 
 exit $?
 
