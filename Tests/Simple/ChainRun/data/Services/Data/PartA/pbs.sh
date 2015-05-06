7ba1b61605f9137519b54f2a137ac403fae29b55
Konrad Klimaszewski <kklimaszewski@cis.gov.pl>
2015-05-06 15:01:20 +0200

    Merged AppServer Postgres branch.

--- Tests/Simple/ChainRun/data/Services/Data/PartA/pbs.sh
+++ Tests/Simple/ChainRun/data/Services/Data/PartA/pbs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+{% if CIS_QUEUE is defined %}
 #PBS -q @@{CIS_QUEUE}
+{% endif %}
 
 echo "Hello World from PartA" > output_A.dat
 sleep 2
--- Tests/Simple/ChainRun/data/Services/Data/PartB/pbs.sh
+++ Tests/Simple/ChainRun/data/Services/Data/PartB/pbs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+{% if CIS_QUEUE is defined %}
 #PBS -q @@{CIS_QUEUE}
+{% endif %}
 
 echo "Hello World from PartB" > output_B.dat
 echo "Message from PartA:" >> output_B.dat
